dce9aaf846ed1d4572731b60918cdda3871daa16
Nicole Roy <nicolesroy@icloud.com>
2022-12-28 06:17:08 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Start the Yubikey Manager on MacOS with the proper Python path
+
+## Dependencies
+- Install the YubiKey Manager app on MacOS
+
+## Running
+`./yubiman-wrapper.sh`
---
+++ yubiman-wrapper.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+export PYTHONPATH=/Applications/YubiKey\ Manager.app/Contents/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages
+/usr/bin/open -a YubiKey\ Manager.app
